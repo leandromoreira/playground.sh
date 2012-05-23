1058f957344ee63cd4bac3d8ab206b00b1bc81df
Leandro Moreira <leandro.ribeiro.moreira@gmail.com>
2012-05-22 23:34:52 -0300

    add file list and manipulation over for loop

--- io.sh
+++ io.sh
@@ -13,3 +13,17 @@ cat hello_world.txt
 #or I can use it like a parameter
 wc < hello_world.txt
 
+#lets iterate over all files inside /bin
+bins=/bin/*
+
+for file in $bins 
+do
+  echo $file
+done
+
+#how about handle only the .sh in current path
+for file in *.sh
+do
+  echo "this is shell => $file, isnt?"
+done
+
