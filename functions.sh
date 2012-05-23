63f1e56092c7aac3f07fe8b0eca3fe3da36f5536
Leandro Moreira <leandro.ribeiro.moreira@gmail.com>
2012-05-23 00:41:39 -0300

    added silly function and import this function in other shell script file

---
+++ functions.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#this is simple function which expect one parameter
+function take {
+  mkdir $1
+  cd $1
+}
+
--- misc.sh
+++ misc.sh
@@ -5,3 +5,14 @@ echo `pwd`
 (cd ~/.vim && ls)
 echo `pwd`
 
+#connecting importing functions or other files on shell programming
+. functions.sh
+take new_folder
+echo `pwd`
+
+#we can create connect commands like that
+LS="ls"
+LS_FLAGS="-al"
+
+$LS $LS_FLAGS $HOME
+
