f40ab3fb0feb13c0b892f2384fae7c60a5c2a3a2
Leandro Moreira <leandro.ribeiro.moreira@gmail.com>
2012-05-23 00:19:13 -0300

    add string operations like subtring and replace and also it introduces the range type

--- variables.sh
+++ variables.sh
@@ -7,3 +7,16 @@ for f in /usr/bin/* ; do
 done
 echo "Google voice: you have $n files"
 
+#we can handle substring too
+string="0123456789"
+echo "from 1 with 3 of size ${string:1:3}"
+
+#now lets replace text inside a string
+msg="this is a worst game Ive ever played"
+echo ${msg/worst/best}
+
+#some modern language does not provide range type
+#shell does
+echo {a..z}
+echo {0..100}
+
